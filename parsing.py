6e967096a51a320c79d3a827482f371fd66efebd
ph <hue0704@naver.com>
2021-01-28 14:46:10 +0900

    정리정돈과 특수문자 제거

--- parsing.py
+++ parsing.py
@@ -6,6 +6,8 @@ import html
 import re
 import xml.etree.ElementTree as ET
 import operator
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Functions
 def load_poison_info(cid):
@@ -36,14 +38,14 @@ f = open('XML/log_output','r')
 cid_list = f.readlines()
 f.close()
 
-## 2. count
+## 2. count words
 count_dic = {}
 for idx in cid_list:
     name, tox = load_poison_info(int(idx))
     lines = ''.join(tox)
     count_dic[idx] = lines.count(':')
 
-  #2-1 
+## 2-1. tot_txt_list 
 tox_txt_list = []
 for idx in count_dic.keys():
     val = count_dic[idx]
@@ -53,40 +55,43 @@ for idx in count_dic.keys():
             tline = tox[tid]
             tox_txt_list.append([idx, tline])
 
-#print(tox_txt_list)
+## 3. text sum
+txt_sum =''
+
 NUM_FM = 0
 NUM_DP = 0
-txt_sum =''
+NUM_LC = 0
+NUM_EC = 0
+
 for tline in tox_txt_list:
-    #print(tline[1])
     NUM_FM += tline[1].lower().count("fathead")
     NUM_DP += tline[1].lower().count("daphnia")
+    NUM_LC += tline[1].lower().count("lc50")
+    NUM_EC += tline[1].lower().count("ec50")
     txt_sum += " }{ "
     txt_sum += tline[1]
-print(NUM_FM)
-print(NUM_DP)
-print(len(tox_txt_list))
-  #2-2
-#print(txt_sum)
 
+print("# of fathead :",NUM_FM)
+print("# of daphnia :",NUM_DP)
+print("# of LC50 :",NUM_LC)
+print("# of EC50 :",NUM_EC)
+print("# of total tox infos :",len(tox_txt_list))
+
+
+## 4. text split & dictionary making
 words = txt_sum.lower().split()
 word_counts = dict()
 
 for word in words:
+    word = word.strip(" ,.;'()/|") 
     word_counts[word] = word_counts.get(word, 0) + 1
     
 sdict = sorted(word_counts.items(), key=operator.itemgetter(1))
+#print(sdict)
 
-print(sdict)
+## 5. histo
 
-
-
-#    if test == 20 : break
-
-## 3. histo
-#import matplotlib.pyplot as plt
-#import numpy as np
-
+## 5-1. count histogram images
 #g = open('histogram.txt','w')
 #bins = np.arange(0,100,1)
 #hist, bins = np.histogram(list(count_dic.values()),bins)
@@ -102,3 +107,20 @@ print(sdict)
 #plt.show()
 
 
+## 5-2. word split dictionary images
+sdict.reverse() # 내림차순으로 변경
+x_index = np.arange(0,60)
+x = [ tpl[0] for tpl in sdict ]
+y = [ tpl[1] for tpl in sdict ]
+
+# color & choose
+colors = [ 'royalblue' for i in range(0,60) ]
+chsidx = [ 0, 2, 18, 36, 43, 44, 49, 53, 54 ]
+for id in chsidx : colors[id] = 'tab:red'
+
+#print(colors)
+print( sdict[:60] )
+print( [sdict[id] for id in chsidx] )
+
+plt.bar(x_index,y[:60], color=colors)
+plt.show()

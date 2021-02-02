ebcc2be8389c7be66d4c17fa7d9c12d8dd95d88c
ph <hue0704@naver.com>
2021-02-02 16:34:02 +0900

    use dif

--- parsing.py
+++ parsing.py
@@ -8,75 +8,44 @@ import xml.etree.ElementTree as ET
 import operator
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Functions
-def load_poison_info(cid):
-    xml_file = os.path.join('XML','output.{}.xml'.format(cid))
-
-    xml_data = ET.parse(xml_file).getroot()
-    str_data = ET.tostring(xml_data, encoding='utf8').decode('utf8')
-
-    name_start_str = '<ns0:RecordTitle>'
-    name_end_str = name_start_str.replace('ns0', '/ns0')
-    regex = re.compile(r'{}.*{}'.format(name_start_str, name_end_str))
-    name = regex.findall(str_data)[0]
-    name = name.replace(name_start_str, '').replace(name_end_str, '')
-
-    start_str = '<ns0:String>'
-    end_str = start_str.replace('ns0', '/ns0')
-    regex = re.compile(r'{}.*{}'.format(start_str, end_str))
-
-    toxic_list = regex.findall(str_data)
-    toxic_list = [html.unescape(i.replace(start_str, '').replace(end_str, ''))
-                   for i in toxic_list]
-
-    return name, toxic_list
-
+import parsing_def as ps
 
 ## 1. output cid list
 f = open('XML/log_output','r')
 cid_list = f.readlines()
 f.close()
 
-## 2. count words
-count_dic = {}
-for idx in cid_list:
-    name, tox = load_poison_info(int(idx))
-    lines = ''.join(tox)
-    count_dic[idx] = lines.count(':')
+## 2. total tox infos
+total_tox = []
 
-## 2-1. tot_txt_list 
-tox_txt_list = []
-for idx in count_dic.keys():
-    val = count_dic[idx]
-    if val == 0 :
-        name, tox = load_poison_info(int(idx))
-        for tid in range(len(tox)):
-            tline = tox[tid]
-            tox_txt_list.append([idx, tline])
+for cid in cid_list:
+    name, tox = ps.load_poison_info(int(cid))
+    total_tox += [ [cid,info.lower()] for info in tox ]
 
-## 3. text sum
-txt_sum =''
 
-NUM_FM = 0
-NUM_DP = 0
-NUM_LC = 0
-NUM_EC = 0
+## 2-1. 
+lc50_tox = ps.word_tox_list("lc50", total_tox)
+ec50_tox = ps.word_tox_list("ec50", total_tox)
+daphnia_tox = ps.word_tox_list("daphnia", total_tox)
 
-for tline in tox_txt_list:
-    NUM_FM += tline[1].lower().count("fathead")
-    NUM_DP += tline[1].lower().count("daphnia")
-    NUM_LC += tline[1].lower().count("lc50")
-    NUM_EC += tline[1].lower().count("ec50")
+print("# of chemicals : ", len(cid_list))
+print("total : ", len(total_tox))
+print("lc50 : ",len(lc50_tox))
+print("ec50 : ",len(lc50_tox))
+print("daphnia : ",len(daphnia_tox))
+
+## 2-2. # of chemicals in lc50_tox
+lc50_cid_list = []
+for tox in lc50_tox:
+    if tox[0] not in lc50_cid_list : lc50_cid_list.append(tox[0])
+
+print(len(lc50_cid_list))
+
+## 3. Word joinging
+txt_sum = ''
+for tox in total_tox:
     txt_sum += " }{ "
-    txt_sum += tline[1]
-
-print("# of fathead :",NUM_FM)
-print("# of daphnia :",NUM_DP)
-print("# of LC50 :",NUM_LC)
-print("# of EC50 :",NUM_EC)
-print("# of total tox infos :",len(tox_txt_list))
-
+    txt_sum += tox[1]
 
 ## 4. text split & dictionary making
 words = txt_sum.lower().split()
@@ -84,30 +53,9 @@ word_counts = dict()
 
 for word in words:
     word = word.strip(" ,.;'()/|") 
-    word_counts[word] = word_counts.get(word, 0) + 1
+    word_counts[word] = word_counts.get(word, 0) + 1 
     
 sdict = sorted(word_counts.items(), key=operator.itemgetter(1))
-#print(sdict)
-
-## 5. histo
-
-## 5-1. count histogram images
-#g = open('histogram.txt','w')
-#bins = np.arange(0,100,1)
-#hist, bins = np.histogram(list(count_dic.values()),bins)
-
-#print(bins)
-#print(hist)
-
-#for index, value in enumerate(hist):
-#    g.write(str(bins[index])+str(hist[index])+"\n")
-
-#g.close()
-#plt.hist(list(count_dic.values()),bins)
-#plt.show()
-
-
-## 5-2. word split dictionary images
 sdict.reverse() # 내림차순으로 변경
 x_index = np.arange(0,60)
 x = [ tpl[0] for tpl in sdict ]
@@ -115,12 +63,22 @@ y = [ tpl[1] for tpl in sdict ]
 
 # color & choose
 colors = [ 'royalblue' for i in range(0,60) ]
-chsidx = [ 0, 2, 18, 36, 43, 44, 49, 53, 54 ]
+chsidx = [ 0, 6, 23, 37, 38, 43, 44, 45, 46 ]
 for id in chsidx : colors[id] = 'tab:red'
 
-#print(colors)
-print( sdict[:60] )
 print( [sdict[id] for id in chsidx] )
-
+#print(sdict[:50])
 plt.bar(x_index,y[:60], color=colors)
 plt.show()
+
+## 4. text split
+#num = 0
+#for tox in lc50_tox :
+#    words = tox[1].split()
+#    for (pos, w) in enumerate(words) :
+#        w.strip(" ,.;'()/|")
+#        if "/l" in w :
+#            if "hardness" in words[pos-2] : num += 1
+            #print( words[pos-4], words[pos-3], words[pos-2], words[pos-1], words[pos] )
+#            break
+#print(num)

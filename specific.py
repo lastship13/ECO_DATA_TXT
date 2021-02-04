247a9f1d65f97989ebff807a65cbd132beb4bac0
jongho <cjh19940724@gmail.com>
2021-02-04 17:24:55 +0900

    csv로 좋은 독성 정보 출력

--- specific.py
+++ specific.py
@@ -1,14 +1,8 @@
 #! /usr/bin/env python
 
 # Modules
-import os
-import html
-import re
-import xml.etree.ElementTree as ET
-import operator
-import matplotlib.pyplot as plt
-import numpy as np
 import parsing_def as ps
+import csv
 
 ## 1. output cid list
 f = open('XML/log_output','r')
@@ -33,32 +27,44 @@ print("lc50 : ",len(lc50_tox))
 print("ec50 : ",len(ec50_tox))
 
 
+#3-1 lc50 good
 tox_list = ps.word_tox_list("species:", lc50_tox)
 tox_list = ps.word_tox_list("concentration:", tox_list)
-
 print("lc50_good:",len(tox_list))
-
 Ncid = ps.count_cid(tox_list)
 print("lc50_good_cid:", Ncid)
-
 cids = ps.mk_cid_list(tox_list)
 c = open('LC50_cid.txt','w')
 for cid in cids:
     c.write(cid)
 c.close()
 
+t = open('LC50_tox.csv', 'w')
+csvwriter = csv.writer(t)
+for dat in tox_list:
+    dat[0] = dat[0].replace('\n','')
+    dat[1] = dat[1].replace('\n','')
+    csvwriter.writerow(dat)
+t.close()
+
+
+#3-2 ec50 good
 tox_list = ps.word_tox_list("species:", ec50_tox)
 tox_list = ps.word_tox_list("concentration:", tox_list)
-
 print("ec50_good:",len(tox_list))
-
 Ncid = ps.count_cid(tox_list)
 print("ec50_good_cid:", Ncid)
-
 cids = ps.mk_cid_list(tox_list)
 c = open('EC50_cid.txt','w')
 for cid in cids:
     c.write(cid)
 c.close()
 
+t = open('EC50_tox.csv', 'w')
+csvwriter = csv.writer(t)
+for dat in tox_list:
+    dat[0] = dat[0].replace('\n','')
+    dat[1] = dat[1].replace('\n','')
+    csvwriter.writerow(dat)
+t.close()
 
